01960b37dd5466a30f3747a5eb144d0b2499ac4c
Raul Otañez <raul10.r77@gmail.com>
2020-02-24 22:46:21 -0600

    Se instalo Composer para descargar la libreria de phpMail para enviar correos con php y se configuro el php.ini con la dirección del archivo mailtodisk.exe en la carpeta mailtodisk de Xampp para recibir los correos en la carpeta de mailoutput

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ require_once "controladores/plantilla_controlador.php";
 
 require_once "controladores/blog_controlador.php";
 require_once "modelos/blog_modelo.php";
-//require_once "modelos/conexion.php";
+
+require_once "controladores/correo_controlador.php";
+
+require_once "extensiones/vendor/autoload.php";
 
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrTraerPlantilla();
