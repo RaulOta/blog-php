79b1b841a0c392ac5e39a1142e4b10c900b81b11
Raul Otañez <raul10.r77@gmail.com>
2020-02-25 21:20:02 -0600

    Instalación del framework Larevel en el proyecto y actualización de la versión de php en el hosting local

--- vistas/paginas/modulos/contenido-sobre-mi.php
+++ vistas/paginas/modulos/contenido-sobre-mi.php
@@ -36,56 +36,56 @@
                             
                             echo '<script>
 
-									if( window.history.replaceState ){
+								if( window.history.replaceState ){
 
-										window.history.replaceState( null, null, window.location.href );
-
-									}
-
-                                </script>';
-                                
-                                if ($enviarCorreo == "ok") {
-									
-									echo '<script>
-
-										notie.alert({
-											type: 1,
-											text: "El mensaje ha sido enviado correctamente, espere muy pronto una respuesta",
-											time: 10
-										})
-											
-									</script>';
+									window.history.replaceState( null, null, window.location.href );
 
 								}
 
-								if ($enviarCorreo == "error") {
-									
-									echo '<script>
-									
-										notie.alert({
-											type: 3,
-											text: "No se pudo enviar el mensaje, inténtelo nuevamente",
-											time: 10
-										})
-											
-									</script>';
-
-                                }
+                            </script>';
                                 
-                                if ($enviarCorreo == "error-sintaxis") {
+                            if ($enviarCorreo == "ok") {
 									
-									echo '<script>
-									
-										notie.alert({
-											type: 3,
-											text: "Error, no se permiten caracteres especiales, inténtelo nuevamente",
-											time: 10
-										})
+								echo '<script>
+
+									notie.alert({
+										type: 1,
+										text: "El mensaje ha sido enviado correctamente, espere muy pronto una respuesta",
+										time: 10
+									})
 											
-									</script>';
+								</script>';
 
-								}
+							}
 
+							if ($enviarCorreo == "error") {
+									
+								echo '<script>
+									
+									notie.alert({
+										type: 3,
+										text: "No se pudo enviar el mensaje, inténtelo nuevamente",
+										time: 10
+									})
+											
+								</script>';
+
+                            }
+                                
+                            if ($enviarCorreo == "error-sintaxis") {
+									
+								echo '<script>
+									
+									notie.alert({
+										type: 3,
+										text: "Error, no se permiten caracteres especiales, inténtelo nuevamente",
+										time: 10
+									})
+											
+								</script>';
+
+                            }
+                            
                         }
 
                     ?>
