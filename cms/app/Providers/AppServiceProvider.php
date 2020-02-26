8e2f45aa44aadef9df2bba73163c41459720797f
Raul Otañez <raul10.r77@gmail.com>
2020-02-25 23:35:29 -0600

    Se agrego el nombre de la base de datos, el usuario y contraseña en el archivo .env, bases de datos con Laravel agregando las tablas que trae por defecto Laravel a la base de datos (blog-php) con el comando (php artisan migrate) y se corrigio/parcho el error generado borrando las tablas creadas en el primer intento y volviendo a crear las tablas con los archivos modificados.

--- cms/app/Providers/AppServiceProvider.php
+++ cms/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        //\Illuminate\Support\Facades\Schema::defaultStringLength(191);
+        Schema::defaultStringLength(191);
     }
 }
