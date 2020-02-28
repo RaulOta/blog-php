4681d62121537e9a0ee465ea0b2209d6921fb2f6
Raul Otañez <raul10.r77@gmail.com>
2020-02-28 12:27:47 -0600

    Ajustes a la cabecera y al pie de página(fooder) de la plantilla

--- cms/resources/views/modulos/footer.blade.php
+++ cms/resources/views/modulos/footer.blade.php
@@ -1,7 +1,14 @@
 <footer class="main-footer">
+
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.0.0
+      Todos los derechos reservados
     </div>
-    <strong>Copyright © 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
-    reserved.
+
+    <strong>Copyright ©
+    @php
+      echo date("Y");
+    @endphp
+
+    <a href="{{ substr(url("/"),0,-11) }}">Juanito Viajero</a>.</strong>
+    
   </footer>
