fea735706eaaa9c07b7fa30a47ab3318a33c39c9
Raul Otañez <raul10.r77@gmail.com>
2020-03-20 22:04:16 -0600

    Creación del Modal para editar usuario administrador

--- cms/app/Http/Controllers/AdministradoresController.php
+++ cms/app/Http/Controllers/AdministradoresController.php
@@ -8,12 +8,38 @@ use App\Blog;
 
 class AdministradoresController extends Controller
 {
+
+    /*============================================
+    Mostrar todos los registros
+    ============================================*/
+
     public function index(){
 
         $administradores = Administradores::all();
         $blog = Blog::all();
 
         return view("paginas.administradores", array("administradores"=>$administradores, "blog"=>$blog));
+
+    }
+
+    /*============================================
+    Mostrar un solo registros
+    ============================================*/
+
+    public function show($id){
+
+        $administradores = Administradores::where("id", $id)->get();
+        $blog = Blog::all();
+
+        if (count($administradores) != 0) {
+            
+            return view("paginas.administradores", array("status"=>200, "administradores"=>$administradores, "blog"=>$blog));
+            
+        }else{
+
+            return view("paginas.administradores", array("status"=>404, "blog"=>$blog));
+
+        }
 
     }
 }
--- cms/resources/views/paginas/administradores.blade.php
+++ cms/resources/views/paginas/administradores.blade.php
@@ -111,11 +111,11 @@
 
                         <div class="btn-group">
 
-                          <button class="btn btn-warning btn-sm">
+                          <a href="{{url('/')}}/administradores/{{$value["id"]}}" class="btn btn-warning btn-sm">
 
                             <i class="fas fa-pencil-alt text-white"></i>
 
-                          </button>
+                          </a>
 
                           <button class="btn btn-danger btn-sm">
 
@@ -268,5 +268,185 @@
     </div>
 
   </div>
+
+  <!--=======================================
+  Editar Administradores (Modal)
+  ========================================-->
+
+  @if (isset($status))
+
+    @if ($status == 200)
+
+      @foreach ($administradores as $key => $value)
+
+        <div class="modal" id="editarAdministradores">
+
+          <div class="modal-dialog">
+      
+            <div class="modal-content">
+      
+              <form method="POST" action="{{ route('register') }}">
+                @csrf
+      
+                <div class="modal-header bg-info">
+      
+                  <h4 class="modal-title">Editar Administradores</h4>
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+      
+                </div>
+      
+                <div class="modal-body">
+      
+                  {{-- Nombre --}}
+      
+                  <div class="input-group mb-3">
+      
+                    <div class="input-group-append input-group-text">
+                      <i class="fas fa-user"></i>
+                    </div>
+      
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $value["name"] }}" required autocomplete="name" autofocus placeholder="Nombre">
+      
+                    @error('name')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                    @enderror
+      
+                  </div>
+      
+                  {{-- Email --}}
+      
+                  <div class="input-group mb-3">
+      
+                    <div class="input-group-append input-group-text">
+                      <i class="fas fa-envelope"></i>
+                    </div>
+      
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $value["email"] }}" required autocomplete="email" placeholder="Email">
+      
+                    @error('email')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                    @enderror
+                    
+                  </div>
+      
+                  {{-- Password --}}
+      
+                  <div class="input-group mb-3">
+      
+                    <div class="input-group-append input-group-text">
+                      <i class="fas fa-key"></i>
+                    </div>
+      
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Contraseña mínimo de 8 caracteres">
+      
+                    @error('password')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                    @enderror
+                    
+                  </div>
+
+                  <input type="hidden" name="password_actual" value="{{ $value["password"] }}">
+
+                  {{-- Rol --}}
+
+                  <div class="input-group mb-3">
+
+                    <div class="input-group-append input-group-text">
+                      <i class="fas fa-list-ul"></i>
+                    </div>
+
+                    <select class="form-control" name="rol" required>
+
+                      @if ($value["rol"] == "administrador" || $value["rol"] == "")
+
+                        <option value="administrador">administrador</option>
+                        <option value="editor">editor</option>
+                      
+                      @else
+
+                        <option value="editor">editor</option>
+                        <option value="administrador">administrador</option>
+                          
+                      @endif
+
+                    </select>
+
+                  </div>
+
+                  {{-- Foto --}}
+                  <hr class="pb-2">
+                  <div class="form-group my-2 text-center">
+
+                    <div class="btn btn-default btn-file">
+
+                      <i class="fas fa-paperclip"></i> Adjuntar Foto
+
+                      <input type="file" name="foto">
+
+                    </div>
+
+                    <br>
+
+                    @if ($value["foto"] == "")
+
+                      <img src="{{url('/')}}/img/administradores/admin.png" class="previsualizarImg img-fluid py-2 w-25 rounded-circle">
+
+                    @else
+
+                      <img src="{{url('/')}}/{{$value["foto"]}}" class="previsualizarImg img-fluid py-2 w-25 rounded-circle">
+                        
+                    @endif
+
+                    <input type="hidden" value="{{$value["foto"]}}" name="imagen_actual">
+
+                    <p class="help-block small">Dimensiones: 200px * 200px | Peso Max. 2MB | Formato: JPG o PNG</p>
+
+                  </div>
+
+
+      
+                </div>
+      
+                <div class="modal-footer d-flex justify-content-between">
+      
+                  <div>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                  </div>
+      
+                  <div>
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                  </div>
+      
+                </div>
+      
+              </form>
+      
+            </div>
+      
+          </div>
+      
+        </div>
+          
+      @endforeach
+
+      <script>
+
+        $("#editarAdministradores").modal();
+
+      </script>
+
+      @else
+
+      {{$status}}
+        
+    @endif
+      
+  @endif
   
 @endsection
